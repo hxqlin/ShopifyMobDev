6a651b3baf53c9570f12c35a4109bc4bf61a4fc1
Hannah Lin <hxqlin@gmail.com>
2017-09-12 13:04:00 -0700

    Initial commit

--- ShopifyMobDev/ViewController.swift
+++ ShopifyMobDev/ViewController.swift
@@ -7,17 +7,61 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var totalBatzSpentField: UITextField!
+    @IBOutlet weak var totalBronzeBagsSoldField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let path = Bundle.main.path(forResource: "shopify", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
+                let json = JSON(data: data)
+                if json != JSON.null {
+                    let batzTotal = totalBatzSpent(json: json)
+                    totalBatzSpentField.text = String(batzTotal)
+                    let numBags = totalBronzeBagsSold(json: json)
+                    totalBronzeBagsSoldField.text = String(numBags)
+                } else {
+                    print("Could not get json from file, make sure that file contains valid json.")
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        } else {
+            print("Invalid filename/path.")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func totalBatzSpent(json: JSON) -> Double {
+        var total = 0.0
+        for (_, order) in json["orders"] {
+            let customerName = order["billing_address"]["name"].stringValue
+            if (customerName == "Napoleon Batz") {
+                let itemsPrice = order["total_line_items_price"].doubleValue
+                total += itemsPrice
+            }
+        }
+        return total
+    }
+    
+    func totalBronzeBagsSold(json: JSON) -> Int {
+        var bagCount = 0;
+        for (_, order) in json["orders"] {
+            let items = order["line_items"]
+            for (_, item) in items {
+                if (item["title"].stringValue == "Awesome Bronze Bag") {
+                    let quantity = item["fulfillable_quantity"].intValue
+                    bagCount += quantity
+                }
+            }
+        }
+        return bagCount
     }
 
 
